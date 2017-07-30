d724e3e50b20b3ed55a0e16466a40ddaa887bf0d
Reuben Reyes <reuben.a.reyes@gmail.com>
2017-07-29 19:54:43 -0700

    fixed unresponsive behavior

---
+++ js/isInViewport.js
@@ -0,0 +1,13 @@
+function isInViewport( element ) {
+  // get binding rectangle
+  var rect = element.getBoundingClientRect();
+
+  // get html tag
+  var html = document.documentElement;
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <= ( window.innerHeight || html.clientHeight ) &&
+    rect.right <= ( window.innerWidth || html.clientWidth )
+  );
+}
