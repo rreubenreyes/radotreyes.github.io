40ac81291d380d18a2f527a4c749ae336dc764f8
Reuben Reyes <reuben.a.reyes@gmail.com>
2017-08-02 13:54:55 -0700

    fixed smooth scroll

--- js/smoothanchor.js
+++ js/smoothanchor.js
@@ -1,7 +1,9 @@
 $( document ).on( 'click', 'a', function( event ) {
-   if ( this.hash !== "" ) { event.preventDefault(); }
+   if ( this.hash !== "" ) {
+      event.preventDefault();
 
-    $('html, body').animate( {
-        scrollTop: $( $.attr( this.attr( 'href') ) ).offset().top
-    }, 500, 'easeInCubic' );
+      $( 'html, body' ).animate( {
+          scrollTop: $( $.attr( this, 'href' ) ).offset().top
+      }, 500, 'easeInCubic' );
+    }
 });
