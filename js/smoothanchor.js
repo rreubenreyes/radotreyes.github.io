c9214096d7cc8b0b862c55d6c2348070e1969f3b
Reuben Reyes <reuben.a.reyes@gmail.com>
2018-07-05 20:48:24 -0700

    stage repo for overhaul;

--- js/smoothanchor.js
+++
@@ -1,9 +0,0 @@
-$( document ).on( 'click', 'a', function( event ) {
-   if ( this.hash !== "" ) {
-      event.preventDefault();
-
-      $( 'html, body' ).animate( {
-          scrollTop: $( $.attr( this, 'href' ) ).offset().top
-      }, 500, 'easeInCubic' );
-    }
-});
