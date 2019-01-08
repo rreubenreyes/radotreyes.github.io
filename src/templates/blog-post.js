5cde51e2417d687b7ffbc8c98e6ee9d5630c882e
Reuben Reyes <reuben.a.reyes@gmail.com>
2019-01-08 10:12:44 -0800

    create blog post content field, link to gatsby

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,93 +1,119 @@
-// import React from 'react'
-// import Helmet from 'react-helmet'
-// import { Link, graphql } from 'gatsby'
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import Helmet from 'react-helmet'
+import { Link, graphql } from 'gatsby'
 
-// import Bio from '../components/Bio'
-// import Layout from '../components/Layout'
-// import { rhythm, scale } from '../utils/typography'
+import Bio from '../components/Bio'
+import Layout from '../components/Layout'
 
-// class BlogPostTemplate extends React.Component {
-//   render() {
-//     const post = this.props.data.markdownRemark
-//     const siteTitle = this.props.data.site.siteMetadata.title
-//     const siteDescription = post.excerpt
-//     const { previous, next } = this.props.pageContext
+class BlogPostTemplate extends Component {
+  static propTypes = {
+    data: PropTypes.shape({
+      site: PropTypes.shape({
+        siteMetadata: PropTypes.shape({
+          title: PropTypes.string.isRequired,
+        }).isRequired,
+      }).isRequired,
+      contentfulBlogPost: PropTypes.shape({
+        title: PropTypes.string.isRequired,
+        date: PropTypes.string.isRequired,
+        content: PropTypes.shape({
+          internal: PropTypes.shape({
+            content: PropTypes.string.isRequired,
+          }).isRequired,
+        }).isRequired,
+        excerpt: PropTypes.string.isRequired,
+      }).isRequired,
+    }).isRequired,
+    location: PropTypes.object.isRequired,
+  }
 
-//     return (
-//       <Layout location={this.props.location} title={siteTitle}>
-//         <Helmet
-//           htmlAttributes={{ lang: 'en' }}
-//           meta={[{ name: 'description', content: siteDescription }]}
-//           title={`${post.frontmatter.title} | ${siteTitle}`}
-//         />
-//         <h1>{post.frontmatter.title}</h1>
-//         <p
-//           style={{
-//             ...scale(-1 / 5),
-//             display: 'block',
-//             marginBottom: rhythm(1),
-//             marginTop: rhythm(-1),
-//           }}
-//         >
-//           {post.frontmatter.date}
-//         </p>
-//         <div dangerouslySetInnerHTML={{ __html: post.html }} />
-//         <hr
-//           style={{
-//             marginBottom: rhythm(1),
-//           }}
-//         />
-//         <Bio />
+  render() {
+    // const post = this.props.data.markdownRemark
+    // const siteTitle = this.props.data.site.siteMetadata.title
+    // const siteDescription = post.excerpt
+    // const { previous, next } = this.props.pageContext
+    const {
+      data: {
+        site: {
+          siteMetadata: { title: siteTitle },
+        },
+        contentfulBlogPost: {
+          title: postTitle,
+          date: postDate,
+          content: {
+            internal: { content: postContent },
+          },
+          excerpt: postExcerpt,
+        },
+      },
+    } = this.props
+    const { location } = this.props
 
-//         <ul
-//           style={{
-//             display: 'flex',
-//             flexWrap: 'wrap',
-//             justifyContent: 'space-between',
-//             listStyle: 'none',
-//             padding: 0,
-//           }}
-//         >
-//           <li>
-//             {
-//               previous &&
-//               <Link to={previous.fields.slug} rel="prev">
-//                 ← {previous.frontmatter.title}
-//               </Link>
-//             }
-//           </li>
-//           <li>
-//             {
-//               next &&
-//               <Link to={next.fields.slug} rel="next">
-//                 {next.frontmatter.title} →
-//               </Link>
-//             }
-//           </li>
-//         </ul>
-//       </Layout>
-//     )
-//   }
-// }
+    return (
+      <Layout location={location} title={siteTitle}>
+        {/* <Helmet
+          htmlAttributes={{ lang: `en` }}
+          meta={[{ name: `description`, content: siteDescription }]}
+          title={`${post.frontmatter.title} | ${siteTitle}`}
+        /> */}
+        <h1>{postTitle}</h1>
+        <p>{postDate}</p>
+        {/* <div dangerouslySetInnerHTML={{ __html: post.html }} /> */}
+        <div>{postContent}</div>
+        {/* previous/next posts follow */}
+        {/* <ul
+          style={{
+            display: `flex`,
+            flexWrap: `wrap`,
+            justifyContent: `space-between`,
+            listStyle: `none`,
+            padding: 0,
+          }}
+        >
+          <li>
+            {previous && (
+              <Link to={previous.fields.slug} rel="prev">
+                ←
+                {` `}
+                {previous.frontmatter.title}
+              </Link>
+            )}
+          </li>
+          <li>
+            {next && (
+              <Link to={next.fields.slug} rel="next">
+                {next.frontmatter.title}
+                {` `}
+→
+              </Link>
+            )}
+          </li>
+        </ul> */}
+      </Layout>
+    )
+  }
+}
 
-// export default BlogPostTemplate
+export default BlogPostTemplate
 
-// export const pageQuery = graphql`
-//   query BlogPostBySlug($slug: String!) {
-//     site {
-//       siteMetadata {
-//         title
-//         author
-//       }
-//     }
-//     markdownRemark(fields: { slug: { eq: $slug } }) {
-//       id
-//       excerpt
-//       html
-//       frontmatter {
-//         title
-//         date(formatString: "MMMM DD, YYYY")
-//       }
-//     }
-//   }
-// `
+export const pageQuery = graphql`
+  query BlogPostBySlug($slug: String!) {
+    site {
+      siteMetadata {
+        title
+        author
+      }
+    }
+    contentfulBlogPost(slug: { eq: $slug }) {
+      title
+      date(formatString: "YYYY MM DD")
+      excerpt
+      content {
+        internal {
+          content
+        }
+      }
+    }
+  }
+`
