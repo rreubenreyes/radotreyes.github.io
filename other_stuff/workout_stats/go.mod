2d43bdf15f679be319a0cb0ae83f122a4adcc6a5
Reuben Reyes <reuben.a.reyes@gmail.com>
2023-10-21 16:37:12 -0700

    feat(activity_tracker): add scaffolding for Strava API fetcher, mapping code

---
+++ other_stuff/workout_stats/go.mod
@@ -0,0 +1,3 @@
+module github.com/rreubenreyes/workout_stats
+
+go 1.20
---
+++ other_stuff/workout_stats/internal/strava/strava.go
@@ -0,0 +1,80 @@
+package strava
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+)
+
+type SummaryActivity struct {
+	Id string `json:"id,omitempty"`
+}
+
+type DetailedActivity struct {
+	Id          string `json:"id,omitempty"`
+	Name        string `json:"name,omitempty"`
+	SportType   string `json:"sport_type,omitempty"`
+	Description string `json:"description,omitempty"`
+}
+
+type Context struct {
+	accessToken string
+	url         string
+}
+
+func ListActivities(ctx *Context) []SummaryActivity {
+	url := fmt.Sprintf("%s/v3/athlete/activities", ctx.url)
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		// TODO: throw here
+		panic(err)
+	}
+	req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", ctx.accessToken))
+	req.Header.Add("Accept", "application/json")
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		// TODO: throw here
+		panic(err)
+	}
+
+	defer res.Body.Close()
+	body, _ := io.ReadAll(res.Body)
+
+	var a []SummaryActivity
+	err = json.Unmarshal(body, &a)
+	if err != nil {
+		// TODO: throw here
+		panic(err)
+	}
+
+	return a
+}
+
+func GetActivity(ctx *Context, id string) DetailedActivity {
+	url := fmt.Sprintf("%s/v3/athlete/activities/%s", ctx.url, id)
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		// TODO: throw here
+		panic(err)
+	}
+	req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", ctx.accessToken))
+	req.Header.Add("Accept", "application/json")
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		// TODO: throw here
+		panic(err)
+	}
+
+	defer res.Body.Close()
+	body, _ := io.ReadAll(res.Body)
+
+	var a DetailedActivity
+	err = json.Unmarshal(body, &a)
+	if err != nil {
+		// TODO: throw here
+		panic(err)
+	}
+
+	return a
+}
